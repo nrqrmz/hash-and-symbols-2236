ab854642cbfc1f92a864cd2a630fb9339f64887a
Enrique Ramírez Ramos <nrqrmz@gmail.com>
2026-02-12 18:12:59 +0000

    add array hash comparison

---
+++ 06_array_vs_hash.rb
@@ -0,0 +1,10 @@
+cities = [ ["London", "England", "Big Ben"], ["Paris", "France", "Tour Eiffel"] ]
+
+cities[1][2]  
+
+
+cities = {
+  "London" => { "country" => "England", "monument" => "Big Ben" },
+  "Paris" => { "country" => "France", "monument" => "Tour Eiffel" }
+}
+cities["Paris"]["monument"] 
